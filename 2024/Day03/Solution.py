e043088f1ee633b468b21f78e7a31f6f3103de3a
cydknee <14293063+cydknee@users.noreply.github.com>
2024-12-05 07:18:29 +0000

    Add 2024 Day 3

---
+++ 2024/Day03/Solution.py
@@ -0,0 +1,33 @@
+""" AoC 2024 Day 3: Mull It Over """
+import re
+
+FILE_NAME = "Input.txt"
+
+
+def read_file():
+    with open(FILE_NAME, "r") as file:
+        return file.read()
+
+
+def part1(program):
+    return sum([int(x[0]) * int(x[1]) for x in [mul[4:-1].split(',') for mul in re.findall(r'mul\([\d]+,[\d]+\)', program)]])
+
+
+def part2(program):
+    total = 0
+    perform_mul = True
+    for mul in re.findall(r'do\(\)|don\'t\(\)|mul\([\d]+,[\d]+\)', program):
+        if mul == 'do()':
+            perform_mul = True
+        elif mul == "don't()":
+            perform_mul = False
+        else:
+            if perform_mul:
+                x, y = map(int, mul[4:-1].split(','))
+                total += x * y
+    return total
+
+
+if __name__ == '__main__':
+    print(part1(read_file()))  # 185797128
+    print(part2(read_file()))  # 89798695
